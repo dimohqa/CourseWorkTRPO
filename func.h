6071406ac112d5316b2854956d72a28d3cc95ac6
Ecgen <ya.evgeny-1999@yandex.ru>
2018-06-25 23:45:04 +0700

    Add print_mmr in func.c, and rating.txt

--- func.h
+++ func.h
@@ -1,12 +1,17 @@
 #ifndef FUN_H
 #define FUN_H
 
+#include <string.h>
+#define green "\033[1;32m"
+#define RESET "\033[0m"
 #include <stdio.h>
 
-void print_hang (int err, int step);
+void print_mmr(char *nick);
 
 int level(int lev);
 
 void encryption(char word[], char enc_word[]);
+
+void print_hang (int err, int step);
 
 #endif
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main() {
     char word[20], enc_word[20];
+    char nick[20]= {"SoNy"};
     int err = 0, lev = 0;
     //FILE *fp = fopen("words.txt","r");
     printf("Enter level:");
@@ -11,6 +12,7 @@ int main() {
         err = level(lev);
     }
     print_hang(7, 7);
+    print_mmr(nick);
     //fclose(fp);
     return 0;
 }
