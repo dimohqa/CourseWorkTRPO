9fd20502bb175de8a3bd6e44ee90887732102df4
dimohqa99 <mr.roog@yandex.ru>
2018-06-25 23:39:17 +0700

    Added a file with words

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
 int main() {
     char word[20], enc_word[20];
     int err = 0, lev = 0;
-    //FILE *fp = fopen("words.txt","r");
+    FILE *fp = fopen("words.txt","r");
+    if (fp == NULL) {
+        printf("Error read file");
+        return 0;
+    }
     printf("Enter level:");
     while (err == 0) {
         scanf("%d", &lev);
         err = level(lev);
     }
     print_hang(7, 7);
-    //fclose(fp);
+    fclose(fp);
+    printf("\n");
     return 0;
 }
